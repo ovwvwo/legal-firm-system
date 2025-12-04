431566d1ab088c4f12a4b60efc82da5b2c570eae
Оляля <O_o_kiseleva@mail.ru>
2025-12-04 19:09:51 +0300

    new html files

--- src/main/java/ru/fa/legal/LegalFirmApplication.java
+++ src/main/java/ru/fa/legal/LegalFirmApplication.java
@@ -1,5 +1,4 @@
 package ru.fa.legal;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,10 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
- * Главный класс приложения информационно-справочной системы юридической фирмы.
- * Точка входа в Spring Boot приложение.
- *
- * @SpringBootApplication - комбинированная аннотация, включающая:
+  * @SpringBootApplication - комбинированная аннотация, включающая:
  *   - @Configuration: помечает класс как источник конфигурации Spring
  *   - @EnableAutoConfiguration: включает автоматическую конфигурацию Spring Boot
  *   - @ComponentScan: сканирует пакет на наличие Spring компонентов
@@ -29,13 +25,11 @@ public class LegalFirmApplication {
      * @param args аргументы командной строки
      */
     public static void main(String[] args) {
-        // SpringApplication.run() инициализирует Spring контекст
-        // и запускает встроенный Tomcat сервер
         SpringApplication.run(LegalFirmApplication.class, args);
         System.out.println("=================================================");
         System.out.println("Информационно-справочная система юридической фирмы");
         System.out.println("Приложение успешно запущено!");
-        System.out.println("Доступно по адресу: http://localhost:8080");
+        System.out.println("Доступно по адресу: http://localhost:9090");
         System.out.println("=================================================");
     }
 
@@ -46,10 +40,10 @@ public class LegalFirmApplication {
      * @Bean - помечает метод как создатель Spring bean
      * @return экземпляр PasswordEncoder
      */
-    @Bean
-    public PasswordEncoder passwordEncoder() {
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
         // Создаем и возвращаем BCryptPasswordEncoder
         // Он автоматически генерирует соль и хеширует пароли
-        return new BCryptPasswordEncoder();
-    }
+       //  return new BCryptPasswordEncoder();
+   // }
 }
